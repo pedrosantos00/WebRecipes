12429cbee036b2e9d7814b24b8af508359c157ed
Pedro Santos <62798656+pedrosantos00@users.noreply.github.com>
2023-10-31 13:57:47 +0000

    Update recipe.service.ts

--- src/app/Services/recipe.service.ts
+++ src/app/Services/recipe.service.ts
@@ -11,7 +11,7 @@ import { Observable } from 'rxjs';
 })
 export class RecipeService {
 
-  private baseUrl: string = "https://localhost:7145/Recipe";
+  private baseUrl: string = "recipewebsiteaps.azurewebsites.net/Recipe";
   constructor(private http: HttpClient, private router: Router) { }
 
 
